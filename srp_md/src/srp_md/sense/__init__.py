a3a2a9d3233c6026e2d0bbf19fbf3b35480b43c5
Henry <jihwangk@umich.edu>
2019-08-08 16:44:26 -0400

    SENSOR: Implement fake PoseCNN sensor

--- srp_md/src/srp_md/sense/__init__.py
+++ srp_md/src/srp_md/sense/__init__.py
@@ -3,3 +3,4 @@ from .sense import BaseSensor
 from .fake_sense import FakeSensor
 from .known_example import ExampleSensor
 from .can_tower_sensor import CanTowerSensor
+from .posecnn_sensor import PoseCNNSensor
---
+++ srp_md/src/srp_md/sense/posecnn_sensor.py
@@ -0,0 +1,24 @@
+from __future__ import absolute_import
+from builtins import range
+from . import sense
+import random
+
+
+class PoseCNNSensor(sense.BaseSensor):
+    def __init__(self):
+        self._objs = []
+        self._poses = []
+        self._num_objs = 3
+        objects = ["A", "B", "C", "D", "E", "F", "G"]
+        self._objs = [random.choice(objects) for _ in range(self._num_objs)]
+
+    def simulate_posecnn(self):
+        self._poses = [[random.random() for _ in range(7)] for _ in range(self._num_objs)]
+        return (self._objs, self._poses)
+
+    def process_data(self, data):
+        processed_data = self.simulate_posecnn()
+        return processed_data
+
+
+sense.sensors['posecnn_sensor'] = PoseCNNSensor
