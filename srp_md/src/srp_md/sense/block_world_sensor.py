40898d7c51483aaa99823f84d6b984dfb2a09251
Henry <jihwangk@umich.edu>
2019-08-09 15:42:21 -0400

    SENSE: Implement scene graph in block world sensor

--- srp_md/src/srp_md/sense/block_world_sensor.py
+++ srp_md/src/srp_md/sense/block_world_sensor.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import
-from builtins import range
+from builtins import str, range
 from . import sense
 import random
 import logging
@@ -7,18 +7,40 @@ import logging
 
 class BlockWorldSensor(sense.BaseSensor):
     def __init__(self):
+        # Initialize logger
         self._logger = logging.getLogger(__name__)
-        self._objs = ["A", "B", "C", "D", "E"]
-        self._predicates = ["On", "NextTo"]
-        self._num_objs = 3
+
+        # Initialize basic info
+        self._objs = ["A", "B", "C", "D", "E", "F", "G"]
+        self._predicates = ["disjoint", "in", "on", "contain", "support", "proximity"]
+        self._num_objs = random.randint(1, len(self._objs))
 
     def process_data(self, data):
+        # Initialize scene graph with variable and predicate nodes
+        self._scene_graph = {}
+        self._variable_keys = ["X_" + str(i + 1) for i in range(self._num_objs)]
+        self._predicate_keys = []
+        for i in range(self._num_objs):
+            for j in range(self._num_objs)[i:]:
+                if i == j:
+                    pass
+                else:
+                    self._predicate_keys.append("R_" + str(i + 1) + str(j + 1))
+
+        # Randomly choose objects from object list
         objs_copy = list(self._objs)
         random.shuffle(objs_copy)
-        objects = objs_copy[:self._num_objs]
-        num_predicates = random.choice([1, 2])
-        predicates = [[random.choice(self._predicates), objects[i], objects[i + 1]] for i in range(num_predicates)]
-        return (objects, predicates)
+        for i in range(len(self._variable_keys)):
+            self._scene_graph[self._variable_keys[i]] = objs_copy[i]
+
+        # Randomly assign predicates
+        for i in range(len(self._predicate_keys)):
+            self._scene_graph[self._predicate_keys[i]] = random.choice(self._predicates)
+
+        # Update the number of objects randomly
+        self._num_objs = random.randint(1, len(self._objs))
+
+        return self._scene_graph
 
 
 sense.sensors['block_world_sensor'] = BlockWorldSensor
