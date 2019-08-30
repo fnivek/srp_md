6eac7fe3816957cbf9a2cb2a1227d9759508d7c7
Kevin French <fnivek@gmail.com>
2019-08-30 09:11:46 -0400

    LEARN: Make a file for all factor learners

--- srp_md/src/srp_md/learn/__init__.py
+++ srp_md/src/srp_md/learn/__init__.py
@@ -4,4 +4,4 @@ from .fake_learn import FakeLearner
 from .version_space_learn import VersionSpaceLearner
 from .factor_graph_learn import FactorGraphLearner
 from .factor_graph_learn import FreqFactorLearner
-from .decision_tree_factor_learner import DecisionTreeFactorLearner
+from .factor_learners import DecisionTreeFactorLearner
