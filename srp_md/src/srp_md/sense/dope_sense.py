d1ea2da215c33f1022b562548052f15ee188e0d3
Kevin French <fnivek@gmail.com>
2019-10-03 10:27:26 -0400

    SENSE: Get one image at time from dope

--- srp_md/src/srp_md/sense/dope_sense.py
+++ srp_md/src/srp_md/sense/dope_sense.py
@@ -19,6 +19,10 @@ class DopeSensor(sense.BaseSensor):
         self._goal = None
         self._client = actionlib.SimpleActionClient('dope', DopeAction)
 
+    def get_next_image(self, timeout=5):
+        # Reset goal
+        self._goal = None
+
         # Start ROS subscribers
         image_sub = message_filters.Subscriber(
             '/dope/webcam/image_raw',
@@ -31,16 +35,43 @@ class DopeSensor(sense.BaseSensor):
         ts = message_filters.TimeSynchronizer([image_sub, info_sub], 1)
         ts.registerCallback(self.image_callback)
 
+        # Wait for message with timeout
+        start = rospy.get_rostime()
+        timeout = rospy.Duration(timeout)
+        rate = rospy.Rate(100)
+        while (self._goal is None and (not rospy.is_shutdown()) and
+                                      (timeout == 0 or rospy.get_rostime() - start < timeout)):
+            rate.sleep()
+
+        if self._goal is None:
+            self._logger.error('Failed to get an image within {}s'.format(timeout.to_sec()))
+
+        # Unregister image subscription
+        image_sub.unregister()
+        info_sub.unregister()
+
     def image_callback(self, image, info):
         self._goal = DopeGoal()
         self._goal.image = image
         self._goal.cam_info = info
 
     def process_data(self, data):
+        # Get the current image
+        self.get_next_image()
+        if self._goal is None:
+            return None
+
+        # Get Dope detections
         self._client.send_goal(self._goal)
-        self._client.wait_for_result()
+        self._client.wait_for_result(rospy.Duration(5))
         result = self._client.get_result()
-        self._logger.debug('Result is {}'.format(result))
+        if result is None:
+            self._logger.error('Failed to get result within 5s')
+            return None
+        self._logger.debug('Dope result is {}'.format(result))
+
+        # Build scene graph
+
         return None
 
 
