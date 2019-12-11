11fb2859ab0617e11fbca07a593e539f3cb5bf04
Kevin French <fnivek@gmail.com>
2019-12-11 11:04:51 -0500

    ACT: Don't tuck the arm unless the torso is fully extended

--- srp_md/src/srp_md/act/actions.py
+++ srp_md/src/srp_md/act/actions.py
@@ -6,7 +6,7 @@ Define the actions used in srp_md.
 import py_trees
 
 from behavior_manager.interfaces.manipulation_behavior_new import (TuckWithCondBehavior, HeadMoveBehavior,
-                                                                   FullyCollapseTorso)
+                                                                   FullyCollapseTorso, FullyExtendTorso)
 
 
 def Reset(name):
@@ -15,14 +15,19 @@ def Reset(name):
     Lift the torso, tuck the arm and look strait.
 
     """
+    seq = py_trees.composites.Sequence(name='seq_{}'.format(name))
     root = py_trees.composites.Parallel(
         name='pal_{}'.format(name),
         policy=py_trees.common.ParallelPolicy.SUCCESS_ON_ALL,
         synchronize=True,
         allow_failure=False)
     root.add_children([
-        FullyCollapseTorso('act_{}_collapse_torso'.format(name)),
-        TuckWithCondBehavior('act_{}_tuck_arm'.format(name), tuck_pose='tuck'),
+        seq,
         HeadMoveBehavior('act_{}_look_strait'.format(name), 'MoveStraight')
+    ])
+    seq.add_children([
+        FullyExtendTorso('act_{}_extend_torso'.format(name)),
+        TuckWithCondBehavior('act_{}_tuck_arm'.format(name), tuck_pose='tuck'),
+        FullyCollapseTorso('act_{}_collapse_torso'.format(name))
     ])
     return root
