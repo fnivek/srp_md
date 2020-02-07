fd8c360b87f07285ab7d98604c7625c747fc8792
Kevin French <fnivek@gmail.com>
2020-02-07 18:57:35 -0500

    ACT: Add a move action that tries to go to the first possible pose in a pose array

--- srp_md/include/srp_md/action_servers/move_to_pose_as.h
+++ srp_md/include/srp_md/action_servers/move_to_pose_as.h
@@ -6,6 +6,7 @@
 
 #include "srp_md/actions.h"
 #include "srp_md_msgs/MoveToPoseAction.h"
+#include "srp_md_msgs/MoveToFirstPoseAction.h"
 
 #include <string>
 
@@ -31,6 +32,52 @@ public:
         as_.publishFeedback(feedback_);
         as_.setSucceeded(result_);
         ROS_INFO("Result sent.");
+    }
+
+protected:
+    actionlib::SimpleActionServer<ActionMsg> as_;
+    Act & act_;
+    const std::string name_;
+    FeedbackMsg feedback_;
+    ResultMsg result_;
+};
+
+class MoveToFirstPoseAS {
+    typedef srp_md_msgs::MoveToFirstPoseAction ActionMsg;
+    typedef srp_md_msgs::MoveToFirstPoseGoal GoalMsg;
+    typedef srp_md_msgs::MoveToFirstPoseFeedback FeedbackMsg;
+    typedef srp_md_msgs::MoveToFirstPoseResult ResultMsg;
+
+public:
+    MoveToFirstPoseAS(ros::NodeHandle & nh, Act & act, const std::string & name)
+    : as_(nh, name, boost::bind(&MoveToFirstPoseAS::goalCB, this, _1), false)
+    , act_(act)
+    , name_(name) {
+        as_.start();
+    }
+
+    void goalCB(const typename GoalMsg::ConstPtr & goal) {
+        ROS_INFO("MoveToFirstPoseAS goalCB");
+        int i = 0;
+        for(auto&& pose : goal->poses)
+        {
+            result_.success = act_.move(pose);
+            if(!result_.success)
+            {
+                feedback_.feedback = "Pose " + std::to_string(i) + "failed";
+                as_.publishFeedback(feedback_);
+            }
+            else
+            {
+                as_.setSucceeded(result_);
+                ROS_INFO("Result sent.");
+                return;
+            }
+            ++i;
+        }
+
+        as_.setAborted(result_);
+        ROS_WARN("Failed to go to any pose");
     }
 
 protected:
