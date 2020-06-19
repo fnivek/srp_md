0bf732ddf2ac4376c039142172ecc32c4f172608
Kevin French <fnivek@gmail.com>
2020-06-19 17:59:30 -0400

    LEARN: Adjust decision tree hyperparameters

--- srp_md/src/srp_md/learn/factor_learners.py
+++ srp_md/src/srp_md/learn/factor_learners.py
@@ -203,8 +203,7 @@ class SklearnFactorLearner(object):
 class DecisionTreeFactorLearner(SklearnFactorLearner):
     # TODO(Kevin): Tune the paramaters of the decision tree
     def __init__(self, category):
-        clf = tree.DecisionTreeClassifier(class_weight='balanced', max_depth=3)
-        clf = tree.DecisionTreeClassifier()
+        clf = tree.DecisionTreeClassifier(class_weight='balanced', max_depth=6, min_samples_leaf=10)
         super(DecisionTreeFactorLearner, self).__init__(clf, category)
 
 
